cdf992c604f66e602b863da89c5e4a5628688ed1
ajaydev <ajay.dev@icodebees.com>
2025-07-01 10:23:21 +0530

    first commit

---
+++ userservice/src/main/java/com/dayve/userservice/model/User.java
@@ -0,0 +1,37 @@
+package com.dayve.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ userservice/src/main/java/com/dayve/userservice/model/UserRole.java
@@ -0,0 +1,6 @@
+package com.dayve.userservice.model;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
