e6917c310d1318d5d2008b4d5bd01bf0d9827398
ajay <ajaydev299@gmail.com>
2025-10-25 18:36:21 +0530

    front end syncronization

--- userservice/src/main/java/com/dayve/userservice/controller/UserController.java
+++ userservice/src/main/java/com/dayve/userservice/controller/UserController.java
@@ -11,9 +11,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.oauth2.jwt.Jwt;
-
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
